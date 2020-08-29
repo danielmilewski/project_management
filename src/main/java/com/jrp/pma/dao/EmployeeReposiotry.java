7535e2404ecdf11161de52e954cff820017a3f25
Daniel_Milewski <milewski.daniel@gmail.com>
2020-08-30 00:34:30 +0200

    [change] - added RepositoryRestResource to repository and added Spring-boot-REST to pom

--- src/main/java/com/jrp/pma/dao/EmployeeReposiotry.java
+++ src/main/java/com/jrp/pma/dao/EmployeeReposiotry.java
@@ -6,9 +6,11 @@ import com.jrp.pma.entities.Project;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "apiemployees", path = "apiemployees")
 public interface EmployeeReposiotry extends PagingAndSortingRepository<Employee, Long> {
 
     @Query(nativeQuery = true, value="SELECT e.first_name as firstName, e.last_name as lastName ,COUNT(pe.employee_id) as projectCount " +
--- src/main/java/com/jrp/pma/dao/ProjectRepository.java
+++ src/main/java/com/jrp/pma/dao/ProjectRepository.java
@@ -5,9 +5,11 @@ import com.jrp.pma.entities.Project;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "apiprojects", path = "apiprojects")
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
     @Override
     public List<Project> findAll();
